7e43b48e0019a9ef5d39b1b3890306b1faed4157
Антон Никифоров <anton2001@mail.ru>
2024-01-09 11:57:26 +0300

    work notebook changes

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Q
+
 from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description').filter(
-        Q(is_published=True) &
-        (Q(is_on_main=True) | Q(title__contains='пломбир')))
+        'id', 'title', 'price', 'description').filter(
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
+    )
     context = {'ice_cream_list': ice_cream_list}
     return render(request, template, context)
